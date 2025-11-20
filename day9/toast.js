9da8e51661b6bda28b89fab867bae3aee0f7df7a
Naveenmehta81 <naveenmehta2002@gmail.com>
2025-11-20 12:54:50 +0530

    toast app
    making simple toast app in which i create  a button success error and invalid
    if i click them then notification .

---
+++ day9/toast.js
@@ -0,0 +1,20 @@
+let toastBox  = document.querySelector('#toastBox');
+let successmssg = "succesfullycompleted";
+let errormsg = "error find!";
+let invalid  = "invalid input check again";
+
+
+
+  
+function showtost(msg){
+    let toast = document.createElement('div');
+    toast.classList.add('toast');
+    toast.innerHTML = msg;
+
+    toastBox.appendChild(toast);
+
+
+    setTimeout(() =>{
+        toast.remove();
+    }, 3000);
+}
